deced08eef928fce9c9e0b1a28ada420a5f33b9b
mate.mrse <mate.mrse@intis.hr>
2022-07-22 12:45:20 +0200

    add actions.yml and requirements.txt

--- email_sender.py
+++ email_sender.py
@@ -1,3 +1,4 @@
+import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -16,5 +17,8 @@ def send_email(subject, sender, recipient, text):
 
     with smtplib.SMTP("smtp.office365.com", 587) as server:
         server.starttls()
-        server.login(user=creds.USER, password=creds.PASSWORD)
+        if os.environ["EMAIL_USER"]:
+            server.login(user=os.environ["EMAIL_USER"], password=os.environ["EMAIL_PASSWORD"])
+        else:
+            server.login(user=creds.USER, password=creds.PASSWORD)
         server.sendmail(from_addr=sender, to_addrs=recipient, msg=message.as_string())
