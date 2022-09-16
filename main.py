ec6962f117f66f271446d6ffed34804df2d5aaf4
mate.mrse <mate.mrse@intis.hr>
2022-09-16 16:33:00 +0200

    minor refactoring

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def main():
     start = perf_counter()
     url = "https://stampar.hr/hr/peludna-prognoza"
 
-    # https://medium.com/@mikelcbrowne/running-chromedriver-with-python-selenium-on-heroku-acc1566d161c
     s = Service(ChromeDriverManager().install())
     chrome_options = Options()
     chrome_options.add_argument("--headless")
@@ -28,37 +27,33 @@ def main():
     driver.maximize_window()
     driver.get(url)
 
+    gradovi = ["Zagreb", "Split", "Pula", "Zadar", "Dubrovnik"]
+    date = datetime.datetime.now()
+    if not os.path.exists(f"data/{date.year}/{date.month}"):
+        os.makedirs(f"data/{date.year}/{date.month}")
+
     driver.find_element(By.CSS_SELECTOR, "#perpetuum-cookie-bar .perpetuum-button-dismiss a").click()
 
-    gradovi = ["Zagreb", "Split", "Pula", "Zadar", "Dubrovnik"]
-
-    def nove_biljke_su():
-        # uzmi vrijednosti iz //div[@class='biljka-naslov']//span
+    def nove_biljke():
         _rezultat = driver.find_elements(By.XPATH, "//div[@class='biljka-naslov']//span")
         _biljke = []
         if len(_rezultat) > 0:
             for r in _rezultat:
                 _biljke.append(r.text)
-
         return _biljke
 
-    stare_biljke = []
+    postojece_biljke = []
     for biljka in biljke.Biljka:
-        stare_biljke.append(biljka.value)
+        postojece_biljke.append(biljka.value)
 
     spomenute_biljke = []
-    date = datetime.datetime.now()
-
-    if not os.path.exists(f"data/{date.year}/{date.month}"):
-        os.makedirs(f"data/{date.year}/{date.month}")
-
     for grad in gradovi:
         izbornik = Select(driver.find_element(By.CSS_SELECTOR, "select[id^='edit-title']"))
         izbornik.select_by_visible_text(grad)
         WebDriverWait(driver, 15).until(
             EC.invisibility_of_element_located((By.CSS_SELECTOR, ".ajax-progress-fullscreen")))
         # provjeri ima li novih biljaka
-        spomenute_biljke.append((nove_biljke_su()))
+        spomenute_biljke.append(nove_biljke())
         # provjeri koncentraciju peluda za biljku
         for biljka in biljke.Biljka:
             xpath = (
@@ -74,7 +69,7 @@ def main():
                     writer = csv.writer(f, escapechar=" ", quoting=csv.QUOTE_NONE)
                     writer.writerow([f"{pelud} {date.today()}"])
 
-    # spomenute_biljke je lista lista, sljedeca linija to pretvara u obicnu listu
+    # pretvara listu lista u obicnu listu:
     flat_spomenute_biljke = [item for sublist in spomenute_biljke for item in sublist]
     flat_spomenute_biljke.sort()
 
@@ -84,7 +79,7 @@ def main():
 
     print(res)
     # usporedi sa postojecim popisom
-    razlika = set(res).difference(set(stare_biljke))
+    razlika = set(res).difference(set(postojece_biljke))
     try:
         if len(razlika) > 0:
             mail_body = f"Nove biljke: {razlika}"
