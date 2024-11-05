ce9ad96bc4f0c9898beb6075320c74de0d269808
mate.mrse <mate.mrse@gmail.com>
2024-11-05 13:41:39 +0100

    trying to fix writing to csv

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
 import os
 from datetime import datetime
 
-# URL and paths
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Use GITHUB_WORKSPACE if available (in GitHub Actions), else fall back to local logic
+BASE_DIR = os.getenv("GITHUB_WORKSPACE", os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Define paths based on BASE_DIR
 BASE_URL = "https://stampar.hr/hr/peludna-prognoza"
 DATE = datetime.now()
 DATA_DIR = os.path.join(BASE_DIR, "data", str(DATE.year), str(DATE.month))
