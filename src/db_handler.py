c816ec01c538b775911016244463a7c6e388c978
mate.mrse <mate.mrse@gmail.com>
2024-11-06 16:29:34 +0100

    trying to fix writing to csv

--- src/db_handler.py
+++ src/db_handler.py
@@ -1,8 +1,16 @@
+import os
 import sqlite3
 from src.config import DB_PATH
 
 def setup_db():
     """Set up the pollen database."""
+
+    # Ensure the parent directory for the database exists
+    db_dir = os.path.dirname(DB_PATH)
+    if not os.path.exists(db_dir):
+        os.makedirs(db_dir)
+        print(f"[DEBUG] Created directory: {db_dir}")
+
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
     cursor.execute('''
