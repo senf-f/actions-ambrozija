72c258ecf20376524cd1f30132a478c23e647adb
senf-f <mate.mrse@gmail.com>
2025-01-24 16:53:11 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from datetime import datetime
 
 # Set BASE_DIR to use the environment variable directly, or fall back to local calculation
@@ -7,7 +8,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # Define paths based on BASE_DIR
 BASE_URL = "https://stampar.hr/hr/peludna-prognoza"
 DATE = datetime.now()
-DATA_DIR = os.path.join(BASE_DIR, "data", str(DATE.year), str(DATE.month))
+#DATA_DIR = os.path.join(BASE_DIR, "data", str(DATE.year), str(DATE.month))
+today = datetime.today()
+DATA_DIR = Path("data") / str(today.year) / str(today.month) / str(today.day)
 DB_PATH = os.path.join(BASE_DIR, "db", "pollen_data.db")
 LOG_FILE = os.path.join(BASE_DIR, "logs", "scraping.log")
 
